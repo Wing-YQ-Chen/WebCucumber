7abe6c82807d5cd6550b5c7b204b92e6d8a5c22e
Wing Chen <13059381952@163.com>
2025-02-27 16:44:05 +0800

    ++

--- src/test/java/org/Testing/Web/Runner/TestRunner.java
+++ src/test/java/org/Testing/Web/Runner/TestRunner.java
@@ -2,14 +2,20 @@ package org.Testing.Web.Runner;
 
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "org.Testing.Web.Steps",
-        plugin = {"pretty", "html:reports/cucumber-report.html"},
-        monochrome = true
+        plugin = {"pretty", "html:reports/cucumber-report.html"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
 
